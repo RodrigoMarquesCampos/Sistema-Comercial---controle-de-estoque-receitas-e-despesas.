9dfc33c014920cc0addebed2035558b2e4710328
RodrigoMarquesCampos <digoleeke@gmail.com>
2017-08-06 18:04:55 -0300

    agora vai kasldhflasdfkh

--- ci/application/views/Crud.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$this->load->view('include/header');
-$this->load->view('include/menu');
-if($tela!="") $this->load->view('Telas/'.$tela);
-$this->load->view('include/footer');
--- ci/application/views/Include/menu.php
+++
@@ -1,6 +0,0 @@
-<ul>
-    <li><?php echo anchor('crud/Create','Create')?></li>
-    <li><?php echo anchor('crud/Retrieve','Retrieve')?></li>
-    <li><?php echo anchor('crud/Update','Update')?></li>
-    <li><?php echo anchor('crud/Delete','Delete')?></li>
-</ul>
